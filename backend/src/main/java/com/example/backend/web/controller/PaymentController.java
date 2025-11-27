de5c65497bb98c16e6845eb3d0b67bbd184efa5b
Utsav Kayastha <utsavkayastha@nchl.local>
2025-11-27 15:05:21 +0545

    minor fix: payment controller fixed to utilize ddd

--- backend/src/main/java/com/example/backend/web/controller/PaymentController.java
+++ backend/src/main/java/com/example/backend/web/controller/PaymentController.java
@@ -3,6 +3,7 @@ package com.example.backend.web.controller;
 import com.example.backend.application.dto.payment.PaymentResponseDto;
 import com.example.backend.application.usecase.PaymentUseCase;
 import com.example.backend.web.dto.payment.PaymentCreateRequest;
+import com.example.backend.web.dto.payment.PaymentResponse;
 import com.example.backend.web.mapper.PaymentWebMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,20 +20,18 @@ public class PaymentController {
     this.paymentService = paymentUseCase;
   }
 
-//  @PostMapping
-//  public PaymentResponse createPayment(@RequestBody PaymentCreateRequest req) {
-//    return PaymentWebMapper.toWeb(
-//            paymentService.recordPayment()
-//    );
-//  }
-
   @PostMapping
   public ResponseEntity<PaymentResponseDto> createPayment(@RequestBody PaymentCreateRequest req) {
     return ResponseEntity.ok(paymentService.recordPayment(PaymentWebMapper.toApplication(req)));
   }
 
   @GetMapping("/user/{userId}")
-  public ResponseEntity<List<PaymentResponseDto>> getUserPayments(@PathVariable Long userId) {
-    return ResponseEntity.ok(paymentService.getPaymentsByUser(userId));
+  public ResponseEntity<List<PaymentResponse>> getUserPayments(@PathVariable Long userId) {
+    return ResponseEntity.ok(
+            paymentService.getPaymentsByUser(userId)
+                    .stream()
+                    .map(PaymentWebMapper::toWeb)
+                    .toList()
+            );
   }
 }
